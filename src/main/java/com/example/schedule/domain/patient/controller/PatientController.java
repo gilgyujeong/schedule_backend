8830a7f5e325226698b60d89c7d5464e1b23533e
gilgyujeong <jgg7645@naver.com>
2024-12-09 03:29:13 +0900

    PatientController CORS 추가

--- src/main/java/com/example/schedule/domain/patient/controller/PatientController.java
+++ src/main/java/com/example/schedule/domain/patient/controller/PatientController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/patient")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PatientController {
 
     private final PatientService patientService;
